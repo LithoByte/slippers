9c534014dfe8b066843edcf63552b08e546b8832
Calvin Collins <calvin.john.collins@gmail.com>
2022-02-01 09:44:17 -0500

    changed to Slippers

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "slippers",
+    name: "Slippers",
     platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "slippers",
-            targets: ["slippers"]),
+            name: "Slippers",
+            targets: ["Slippers"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,20 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "slippers",
+            name: "Slippers",
             dependencies: [],
             path: "Sources/Slippers/Classes/Core"),
         .testTarget(
             name: "slippersTests",
-            dependencies: ["slippers"]),
-        .target(
-            name: "slippers/IntId",
-            dependencies: ["slippers"],
-            path: "Sources/Slippers/Classes/Id"),
-        .target(
-            name: "slippers/StringId",
-            dependencies: ["slippers"],
-            path: "Sources/Slippers/Classes/StringId"
-        )
+            dependencies: ["Slippers"])
     ]
 )
